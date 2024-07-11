3726f6ce302691be45237164892f64d49847dc82
Bilal BELKAHLA <b.belkahla@NeoSoft.local>
2024-07-11 16:55:52 +0200

    Cors problem a resoudre

--- src/main/java/com/bba/ebankingbackend/web/ClientRestController.java
+++ src/main/java/com/bba/ebankingbackend/web/ClientRestController.java
@@ -6,6 +6,7 @@ import com.bba.ebankingbackend.dtos.ClientDTO;
 import com.bba.ebankingbackend.exceptions.CustomerNotFoundException;
 import com.bba.ebankingbackend.services.ClientService;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,32 +14,37 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @Slf4j
-@CrossOrigin("*")
 public class ClientRestController {
     private ClientService clientService;
     
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<ClientDTO> customers(){
         return clientService.listCustomers();
     }
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<ClientDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
         return clientService.searchCustomers("%"+keyword+"%");
     }
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public ClientDTO getCustomerById(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return clientService.getCustomer(customerId);
     }
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ClientDTO saveCustomer(@RequestBody ClientDTO ClientDTO){
         return clientService.saveCustomer(ClientDTO);
     }
     @PutMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ClientDTO updateCustomer(@PathVariable Long customerId, @RequestBody ClientDTO clientDTO){
     	clientDTO.setId(customerId);
         return clientService.updateCustomer(clientDTO);
     }
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable Long id){
     	clientService.deleteClient(id);
     }
